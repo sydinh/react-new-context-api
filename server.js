7833f5f8e7d3b82920a664d87f05171d927ac5b0
sydinh <sydinh.dev@gmail.com>
2018-04-01 16:27:39 +0700

    Add server

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+const dirServe = 'build';
+
+app.use('/', express.static(`${__dirname}/${dirServe}`));
+
+// express will serve up index.html if it doesn't recognize the route
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, `${dirServe}`, 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}, Ctrl+C to stop`));
